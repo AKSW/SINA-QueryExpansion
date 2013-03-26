a871f126250866e3a7bcf71b57a576869f5e2492
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2013-03-26 12:04:10 +0100

    Added test case for readTsvCsvToMultiMap(File).

--- test/model/ImportExportTest.java
+++ test/model/ImportExportTest.java
@@ -2,13 +2,27 @@ package model;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.TreeSet;
+
+import org.apache.commons.collections15.MultiMap;
 import org.apache.commons.collections15.multimap.MultiHashMap;
 import org.junit.Test;
 
 public class ImportExportTest
 {
+	@Test
+	public void testReadTsvCsvToMultiMap() throws FileNotFoundException
+	{
+		MultiMap<String,String> map = ImportExport.readTsvCsvToMultiMap(new File("output/qald.tsv"));
+		assertTrue(map.keySet().size()>10);
+		assertTrue(new TreeSet<String>(map.get("spokenin")).toString().equals("[Language, Languages, OfficialLang, language, official language, official languages, region, spoken in]"));
+		assertTrue(new TreeSet<String>(map.get("produce")).toString().equals("[Caption, Name, designer, designer company, manufacturer, model, origin, related, sequence, service, title]"));
+	}
 
+	
 	@Test
 	public void testMultiMapToStringCsvTsv()
 	{
