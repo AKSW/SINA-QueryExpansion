2c2d7acd72a4a92f57af008f8b9f942fb19e46da
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2013-04-24 18:40:21 +0200

    added script to generate lemon dataset and it's source and generated file.

---
+++ src/scripts/CreateNewInputFiles.java
@@ -0,0 +1,79 @@
+package scripts;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections15.MultiMap;
+import org.apache.commons.collections15.map.HashedMap;
+import org.apache.commons.collections15.multimap.MultiHashMap;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.shared.PrefixMapping;
+
+public class CreateNewInputFiles
+{
+	static Model m;
+
+	static ResultSet execSelect(String queryStr)
+	{		
+		Query q = new Query();
+		q.setPrefixMapping(m);
+		QueryFactory.parse(q,queryStr,null,Syntax.syntaxSPARQL);		
+		QueryExecution qe = QueryExecutionFactory.create(q,m);
+		return qe.execSelect();
+	}
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		File f = new File("input/source/dbpedia_train_lexicon_en.ttl");
+
+		//		new FileManager().readModel(m,f.getAbsolutePath(),"TURTLE");
+		m = ModelFactory.createMemModelMaker().createDefaultModel();
+		m.read(new FileInputStream(f),"http://sc.cit-ec.uni-bielefeld.de/lexica/dbpedia#","TURTLE");
+		//		m.setNsPrefix("","http://sc.cit-ec.uni-bielefeld.de/lexica/dbpedia#");
+		//		m.setNsPrefix("lemon","http://www.monnet-project.eu/lemon#");
+
+		ResultSet entrySet = execSelect("select ?s ?l ?d {?s a lemon:LexicalEntry. ?s lemon:canonicalForm ?c." +
+				"?c lemon:writtenRep ?l. ?s lemon:sense ?sense. ?sense lemon:reference ?d.}");
+
+		MultiMap<String,String> wordToSense = new MultiHashMap<>();
+		while(entrySet.hasNext())
+		{
+			QuerySolution qs = entrySet.next();
+			String uri = qs.get("?s").toString();
+			//			System.out.println(uri);
+			String suffix = uri.substring(uri.lastIndexOf('#')+1);
+			//			System.out.println(suffix);
+			String label = qs.get("?l").asLiteral().getLexicalForm();
+			//			System.out.println(label);
+			String sense = qs.get("?d").toString();
+			String senseSuffix = sense.substring(sense.lastIndexOf('/')+1);
+			
+			if(!suffix.equals(senseSuffix))
+			{
+//				System.out.println(suffix+"!="+senseSuffix);
+				wordToSense.put(label,sense.replace("http://dbpedia.org/ontology/","dbo:"));
+			}
+		}
+		new File("tmp").mkdir();
+		try(PrintWriter out = new PrintWriter("tmp/lemon.tsv"))
+		{
+			for(String word: wordToSense.keySet())
+			{
+				out.println(word+"\t"+wordToSense.get(word).toString().replace(", ","\t").replace("[","").replace("]",""));
+			}
+		}
+	}
+}
